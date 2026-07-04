59034a0ee0f8caebd50c71c7db06885957f53579
PARMANAND RAY <156565937+parmanand-ray@users.noreply.github.com>
2026-07-04 16:30:06 +0530

    Modify cookie settings for secure authentication
    Updated cookie settings to use 'sameSite: none' and 'secure: true' for enhanced security.

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -49,8 +49,8 @@ export const signUp = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 2 * 24 * 60 * 60 * 1000,
-      sameSite: "lax",
-      secure: false,
+      sameSite: "none",
+      secure: true,
     });
 
     return res.status(201).json({
@@ -105,8 +105,8 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       maxAge: 2 * 24 * 60 * 60 * 1000,
-      sameSite: "lax",
-      secure: false,
+        sameSite: "none",
+      secure: true,
     });
 
     return res.status(200).json({
